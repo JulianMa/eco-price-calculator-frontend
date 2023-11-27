4bfe587390ee204f40601a64a56e2892efe6d368
JulianMa <julian@maczuga.net>
2023-11-27 17:23:45 +0100

    Remove Hash from emitted Filenames
    Loading Tmp

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,33 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
-  build: {
-    target: 'esnext',
-    polyfillDynamicImport: false,
-  },
+    plugins: [solidPlugin()],
+    build: {
+        target: 'esnext',
+        rollupOptions: {
+            output: {
+                manualChunks: undefined,
+                inlineDynamicImports: true,
+                entryFileNames: "index.js",
+                assetFileNames: `[name].[ext]`
+            }
+        },
+    },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:3001',
+                changeOrigin: true,
+                headers: {
+                    "X-Auth-Token": "i2k4UPeyGVHiHjyLm1pYdMdIlw2g4bPR/b9SfZw9U+w=",
+                    "X-Auth-Token-Type": "slg"
+                }
+            },
+            '/data': {
+                target: 'http://127.0.0.1:3001',
+                changeOrigin: true,
+            },
+        },
+    },
 });
