e0302afc6e4016164b3a34fcf8ab485413092307
JulianMa <julian@maczuga.net>
2023-12-02 17:09:41 +0100

    Only load files from Local DB

--- src/utils/restDbSdk.ts
+++ src/utils/restDbSdk.ts
@@ -2,13 +2,32 @@ import { removeXmlTags } from './helpers';
 import * as constants from './constants';
 
 const endpoints = {
-  readServer: ".netlify/functions/servers",
-  readDB: (serverName: string, fileName: string) => `.netlify/functions/files?server=${serverName}&file=${fileName}`,
+  readServer: '.netlify/functions/servers',
+  readDB: (serverName: string, fileName: string) =>
+    `.netlify/functions/files?server=${serverName}&file=${fileName}`,
 };
 
 async function fetchAsync<T>(url: string): Promise<T | undefined> {
   try {
     const response = await fetch(url);
+    return (await response.json()) as T;
+  } catch (e) {
+    // Swallow error and return undefined
+    // console.log(`Could not fetch from ${url}`);
+  }
+  return undefined;
+}
+
+async function fetchAsyncAuthorized<T>(url: string): Promise<T | undefined> {
+  var authtoken = localStorage.getItem("authtoken") ?? "";
+  var authtokentype = localStorage.getItem("authtokentype") ?? "";
+  try {
+    const response = await fetch(url, {
+      headers: {
+        'X-Auth-Token': authtoken,
+        'X-Auth-Token-Type': authtokentype,
+      },
+    });
     return (await response.json()) as T;
   } catch (e) {
     // Swallow error and return undefined
@@ -23,28 +42,34 @@ export const readDB = (serverName: string, fileName: string) =>
 export const getServers = () =>
   fetchAsync<ServersResponse[]>(endpoints.readServer);
 
-export const getStores = (serverName: string): Promise<StoresResponse | undefined> =>
-  fetchAsync<StoresResponse>("/data/Stores.json").then(
-    (response) =>
-      response
-        ? {
-          ...response,
-          Stores: response?.Stores?.map((store) => ({
-            ...store,
-            Name: removeXmlTags(store.Name),
-          })),
-        }
-        : undefined
+export const getStores = (
+  serverName: string
+): Promise<StoresResponse | undefined> =>
+  fetchAsync<StoresResponse>('/data/Stores.json').then((response) =>
+    response
+      ? {
+        ...response,
+        Stores: response?.Stores?.map((store) => ({
+          ...store,
+          Name: removeXmlTags(store.Name),
+        })),
+      }
+      : undefined
   );
 
-export const getRecipes = (serverName: string): Promise<RecipesResponse | undefined> =>
-  fetchAsync<RecipesResponse>( "/data/Recipes.json");
+export const getRecipes = (
+  serverName: string
+): Promise<RecipesResponse | undefined> =>
+  fetchAsync<RecipesResponse>('/data/Recipes.json');
 
 export const getTags = (serverName: string) =>
-  fetchAsync<TagsResponse>( "/data/Tags.json");
+  fetchAsync<TagsResponse>('/data/Tags.json');
 
 export const getAllItems = (serverName: string) =>
-  fetchAsync<AllItemsResponse>("/data/AllItems.json");
+  fetchAsync<AllItemsResponse>('/data/AllItems.json');
 
 export const getCraftingTables = (serverName: string) =>
-  fetchAsync<CraftingTablesResponse>("/data/CraftingTables.json");
+  fetchAsync<CraftingTablesResponse>('/data/CraftingTables.json');
+
+export const getUsername = (): Promise<{ username: string } | undefined> =>
+  fetchAsyncAuthorized<{ username: string }>('/api/v1/plugins/EcoLiveDataExporter/username');
